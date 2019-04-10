58988b2a939373d4e7c5928852f6661dc81606b4
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-04-10 09:07:37 +0200

    feat: add nameparser

--- src/Untitled-1.ts
+++ src/Untitled-1.ts
@@ -1,7 +1,11 @@
+
+
 import React from 'react';
+import { start } from 'repl';
 
 
-const testAddress= `Marcel Herrera-Becker
+const testAddress= `Marcel
+Herrera-Becker
 Candidate Relationship Manager
 Tel.: +49 211 69540 609
 Fax: +49 211 69540 700
@@ -14,9 +18,9 @@ Geschäftsführer: Andreas Knieper
 Amtsgericht Düsseldorf
 HRB Nr: 75031`
 
-/*it('parses the name' ,() => {
+it('parses the name' ,() => {
     const lines = testAddress.split('\n')
-    expect(lines.length).toBe(12)
+    expect(lines.length).toBe(13)
     const zipNameRegex = new RegExp(/^([ \u00c0-\u01ffa-zA-Z'\-])+$/)
 
     const result = lines.reduce( (currentCount: number, nextLine:string) => {
@@ -26,12 +30,12 @@ HRB Nr: 75031`
     }, 0)
     
     expect(result).toBe(1)
-})*/
+})
 
 
 it('parses the address' ,() => {
     const lines = testAddress.split('\n')
-    expect(lines.length).toBe(12)
+    expect(lines.length).toBe(13)
     const zipCityRegex = new RegExp(/^(?:([a-z|[A-Z]){1,3}[- ]+)?\d{5}\s+(?:[a-z]|[A-Z]|[äöüß]|[ÄÖÜ]|\s|-)*/)
 
     const result = lines.reduce( (currentCount: number, nextLine:string) => {
@@ -45,7 +49,7 @@ it('parses the address' ,() => {
 
 it('parses the email' ,() => {
     const lines = testAddress.split('\n')
-    expect(lines.length).toBe(12)
+    expect(lines.length).toBe(13)
     const zipMailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
     const result = lines.reduce( (currentCount: number, nextLine:string) => {
         const test = zipMailRegex.test(nextLine);
